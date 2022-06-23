3bc49f7dbd77f59b9b190ac3d274760743e3511b
Andre Augusto Ribas <dba.ribas@gmail.com>
2022-06-23 12:37:04 +0200

    Update dbnitro.sh

--- dbnitro.sh
+++ dbnitro.sh
@@ -8,22 +8,36 @@ EMAIL_1="dba.ribas@gmail.com"
 EMAIL_2="andre.ribas@icloud.com"
 WEBSITE="http://dbnitro.net"
 #
-mkdir -p /opt/dbnitro/
-chmod -R 775 /opt/dbnitro/
-chown -R oracle.oinstall /opt/dbnitro/
-cd /opt/dbnitro/
+# ------------------------------------------------------------------------
+# Verify if you are ROOT or not
 #
-wget -O /opt/dbnitro/.OracleMenu.sh https://raw.githubusercontent.com/dbaribas/dbnitro/main/OracleMenu.sh
-wget -O /opt/dbnitro/.Oracle_ASM_Functions https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ASM_Functions
-wget -O /opt/dbnitro/.Oracle_DBA_Functions https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_DBA_Functions
+if [[ "$(whoami)" != "root" ]]; then
+  echo " -- YOU ARE NOT ROOT, YOU MUST BE ROOT TO EXECUTE THIS SCRIPT --"
+  exit 1
+fi
+# ------------------------------------------------------------------------
+# Creating and Installing the DBNITRO Components
 #
-chown oracle.oinstall /opt/dbnitro/.OracleMenu.sh
-chown oracle.oinstall /opt/dbnitro/.Oracle_ASM_Functions
-chown oracle.oinstall /opt/dbnitro/.Oracle_DBA_Functions
+FOLDER="/opt"
 #
-chmod a+x /opt/dbnitro/.OracleMenu.sh
-chmod g+w /opt/dbnitro/.OracleMenu.sh
-chmod 775 /opt/dbnitro/.OracleMenu.sh
+if [[ ! -d ${FOLDER}/dbnitro/ ]]; then
+  mkdir -p ${FOLDER}/dbnitro/
+  chmod -R 775 ${FOLDER}/dbnitro/
+  chown -R oracle.oinstall ${FOLDER}/dbnitro/
+fi
+cd ${FOLDER}/dbnitro/
+#
+wget -O ${FOLDER}/dbnitro/.OracleMenu.sh https://raw.githubusercontent.com/dbaribas/dbnitro/main/OracleMenu.sh
+wget -O ${FOLDER}/dbnitro/.Oracle_ASM_Functions https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ASM_Functions
+wget -O ${FOLDER}/dbnitro/.Oracle_DBA_Functions https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_DBA_Functions
+#
+chown oracle.oinstall ${FOLDER}/dbnitro/.OracleMenu.sh
+chown oracle.oinstall ${FOLDER}/dbnitro/.Oracle_ASM_Functions
+chown oracle.oinstall ${FOLDER}/dbnitro/.Oracle_DBA_Functions
+#
+chmod a+x ${FOLDER}/dbnitro/.OracleMenu.sh
+chmod g+w ${FOLDER}/dbnitro/.OracleMenu.sh
+chmod 775 ${FOLDER}/dbnitro/.OracleMenu.sh
 #
 wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/asmdu.sh
 wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/cell-status.sh
@@ -55,65 +69,56 @@ wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/svc-set-f
 wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/svc-show-config.sh
 wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/yal.sh
 #
-chmod a+x /opt/dbnitro/asmdu.sh
-chmod a+x /opt/dbnitro/cell-status.sh
-chmod a+x /opt/dbnitro/change-password.sh
-chmod a+x /opt/dbnitro/exa-howsmart.sh
-chmod a+x /opt/dbnitro/exa-iblinkinfo.sh
-chmod a+x /opt/dbnitro/exa-racklayout.sh
-chmod a+x /opt/dbnitro/exa-versions.sh
-chmod a+x /opt/dbnitro/gg-afterstop.sh
-chmod a+x /opt/dbnitro/gg-info.sh
-chmod a+x /opt/dbnitro/gg-start.sh
-chmod a+x /opt/dbnitro/gg-status.sh
-chmod a+x /opt/dbnitro/gg-stop.sh
-chmod a+x /opt/dbnitro/list-ohpatches.sh
-chmod a+x /opt/dbnitro/locks.sql
-chmod a+x /opt/dbnitro/lspatches.sh
-chmod a+x /opt/dbnitro/nfs-status.sh
-chmod a+x /opt/dbnitro/oci-check-backups.sh
-chmod a+x /opt/dbnitro/oraenv++
-chmod a+x /opt/dbnitro/pidsid.sql
-chmod a+x /opt/dbnitro/rac-mon.sh
-chmod a+x /opt/dbnitro/rac-on_all_db.sh
-chmod a+x /opt/dbnitro/rac-status-rh6.sh
-chmod a+x /opt/dbnitro/rac-status.sh
-chmod a+x /opt/dbnitro/rac-status_suresh.sh
-chmod a+x /opt/dbnitro/rman-backup.sh
-chmod a+x /opt/dbnitro/sidpid.sql
-chmod a+x /opt/dbnitro/svc-set-failback-yes.sh
-chmod a+x /opt/dbnitro/svc-show-config.sh
-chmod a+x /opt/dbnitro/yal.sh
+chmod a+x ${FOLDER}/dbnitro/*.sh
+chmod a+x ${FOLDER}/dbnitro/oraenv++
 #
-chown oracle.oinstall /opt/dbnitro/asmdu.sh
-chown oracle.oinstall /opt/dbnitro/cell-status.sh
-chown oracle.oinstall /opt/dbnitro/change-password.sh
-chown oracle.oinstall /opt/dbnitro/exa-howsmart.sh
-chown oracle.oinstall /opt/dbnitro/exa-iblinkinfo.sh
-chown oracle.oinstall /opt/dbnitro/exa-racklayout.sh
-chown oracle.oinstall /opt/dbnitro/exa-versions.sh
-chown oracle.oinstall /opt/dbnitro/gg-afterstop.sh
-chown oracle.oinstall /opt/dbnitro/gg-info.sh
-chown oracle.oinstall /opt/dbnitro/gg-start.sh
-chown oracle.oinstall /opt/dbnitro/gg-status.sh
-chown oracle.oinstall /opt/dbnitro/gg-stop.sh
-chown oracle.oinstall /opt/dbnitro/list-ohpatches.sh
-chown oracle.oinstall /opt/dbnitro/locks.sql
-chown oracle.oinstall /opt/dbnitro/lspatches.sh
-chown oracle.oinstall /opt/dbnitro/nfs-status.sh
-chown oracle.oinstall /opt/dbnitro/oci-check-backups.sh
-chown oracle.oinstall /opt/dbnitro/oraenv++
-chown oracle.oinstall /opt/dbnitro/pidsid.sql
-chown oracle.oinstall /opt/dbnitro/rac-mon.sh
-chown oracle.oinstall /opt/dbnitro/rac-on_all_db.sh
-chown oracle.oinstall /opt/dbnitro/rac-status-rh6.sh
-chown oracle.oinstall /opt/dbnitro/rac-status.sh
-chown oracle.oinstall /opt/dbnitro/rac-status_suresh.sh
-chown oracle.oinstall /opt/dbnitro/rman-backup.sh
-chown oracle.oinstall /opt/dbnitro/sidpid.sql
-chown oracle.oinstall /opt/dbnitro/svc-set-failback-yes.sh
-chown oracle.oinstall /opt/dbnitro/svc-show-config.sh
-chown oracle.oinstall /opt/dbnitro/yal.sh
+chown -R oracle.oinstall ${FOLDER}/dbnitro/
+#
+# ------------------------------------------------------------------------
+# Add the Content on Grid Profile
+#
+cat > /home/grid/.bash_profile <<EOF
+# .bash_profile
+
+# Get the aliases and functions
+if [[ -f ~/.bashrc ]]; then
+       . ~/.bashrc
+fi
+#
+# User specific environment and startup programs
+#
+export PATH=/usr/local/bin:/bin:/sbin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/grid/.local/bin:/home/grid/bin
+export PS1=\$'[ \${LOGNAME}@\h:\$(pwd): ]\$ '
+#
+echo " -- TO SELECT ANY ORACLE PRODUCT, JUST TYPE: db --"
+echo " -- IT WILL SHOW YOU ALL OPTIONS YOU CAN USE --"
+alias db='. ${FOLDER}/dbnitro/.OracleMenu.sh'
+#
+umask 0022
+EOF
+#
+# ------------------------------------------------------------------------
+# Add the Content on Oracle Profile
+#
+cat > /home/oracle/.bash_profile <<EOF
+# .bash_profile
+
+# Get the aliases and functions
+if [[ -f ~/.bashrc ]]; then
+       . ~/.bashrc
+fi
+#
+# User specific environment and startup programs
+#
+export PATH=/usr/local/bin:/bin:/sbin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/oracle/.local/bin:/home/oracle/bin
+export PS1=\$'[ \${LOGNAME}@\h:\$(pwd): ]\$ '
+#
+echo " -- TO SELECT ANY ORACLE PRODUCT, JUST TYPE: db --"
+echo " -- IT WILL SHOW YOU ALL OPTIONS YOU CAN USE --"
+alias db='. ${FOLDER}/dbnitro/.OracleMenu.sh'
+#
+umask 0022
+EOF
 #
 # --------------//--------------//--------------//--------------//--------------//--------------//--------------//-----
 # THE SCRIPT FINISHES HERE
