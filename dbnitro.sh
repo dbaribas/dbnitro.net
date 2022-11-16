cb4ed77f6ea36f0984edf56a1a1579338a35539f
Andre Augusto Ribas <dba.ribas@gmail.com>
2022-11-16 13:06:52 +0100

    Update dbnitro.sh

--- dbnitro.sh
+++ dbnitro.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 Author="Andre Augusto Ribas"
-SoftwareVersion="1.0.3"
+SoftwareVersion="1.0.5"
 DateCreation="22/06/2022"
-DateModification="15/09/2022"
+DateModification="16/11/2022"
 EMAIL_1="dba.ribas@gmail.com"
 EMAIL_2="andre.ribas@icloud.com"
 WEBSITE="http://dbnitro.net"
@@ -39,7 +39,7 @@ SepLine
 # ------------------------------------------------------------------------
 # Creating and Installing the DBNITRO Components
 #
-FOLDER="/opt"
+FOLDER="/opt"                       # ===> HERE YOU HAVE TO CONFIGURE THE PATH OF DBNITRO, WHERE IT WILL BE INSTALLED
 DBNITRO="${FOLDER}/dbnitro"
 #
 function RemoveFolder() {
@@ -51,68 +51,72 @@ fi
 function CreateFolder() {
 if [[ ! -d ${DBNITRO}/ ]]; then
   mkdir -p ${DBNITRO}/
+  mkdir -p ${DBNITRO}/bin/
+  mkdir -p ${DBNITRO}/var/
   mkdir -p ${DBNITRO}/reports/
+  mkdir -p ${DBNITRO}/functions/
+  mkdir -p ${DBNITRO}/statements/
 fi
 }
 #
 function SetUpDBNITRO() {
 cd ${DBNITRO}/
+# ===> Here is the DBNITRO Files
+wget -O ${DBNITRO}/bin/OracleMenu.sh                                             https://raw.githubusercontent.com/dbaribas/dbnitro/main/OracleMenu.sh
+wget -O ${DBNITRO}/bin/Oracle_DBA_Check_Hugepages.sh                             https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_DBA_Check_Hugepages.sh
+wget -O ${DBNITRO}/bin/GoldenGateMonitor.sh                                      https://raw.githubusercontent.com/dbaribas/dbnitro/main/GoldenGateMonitor.sh
+wget -O ${DBNITRO}/functions/Oracle_ASM_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ASM_Functions
+wget -O ${DBNITRO}/functions/Oracle_DBA_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_DBA_Functions
+wget -O ${DBNITRO}/functions/Oracle_RAC_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_RAC_Functions
+wget -O ${DBNITRO}/functions/Oracle_EXA_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_EXA_Functions
+wget -O ${DBNITRO}/functions/Oracle_ODG_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ODG_Functions
+wget -O ${DBNITRO}/functions/Oracle_OGG_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_OGG_Functions
+wget -O ${DBNITRO}/functions/Oracle_STR_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_STR_Functions
+wget -O ${DBNITRO}/functions/Oracle_PDB_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_PDB_Functions
+wget -O ${DBNITRO}/functions/Oracle_ODA_Functions                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ODA_Functions
+wget -O ${DBNITRO}/functions/Oracle_WALL_Functions                               https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_WALL_Functions
+wget -O ${DBNITRO}/functions/Oracle_RMAN_Functions                               https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_RMAN_Functions
+wget -O ${DBNITRO}/statements/Oracle_SQL_DBA_Info.sql                            https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_DBA_Info.sql
+wget -O ${DBNITRO}/statements/Oracle_SQL_Report_v.3.0.1.sql                      https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_Report_v.3.0.1.sql
+wget -O ${DBNITRO}/statements/Oracle_SQL_Check_Hugepages.sql                     https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_Check_Hugepages.sql
+wget -O ${DBNITRO}/statements/Oracle_SQL_Options_Packs_Usage_Statistics.sql      https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_Options_Packs_Usage_Statistics.sql
+# ===> Here is the Fred Denis Scripts
+wget -o ${DBNITRO}/bin/asmdu.sh                                                  https://raw.githubusercontent.com/freddenis/oracle-scripts/master/asmdu.sh
+wget -o ${DBNITRO}/bin/cell-status.sh                                            https://raw.githubusercontent.com/freddenis/oracle-scripts/master/cell-status.sh
+wget -o ${DBNITRO}/bin/change-password.sh                                        https://raw.githubusercontent.com/freddenis/oracle-scripts/master/change-password.sh
+wget -o ${DBNITRO}/bin/exa-howsmart.sh                                           https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-howsmart.sh
+wget -o ${DBNITRO}/bin/exa-iblinkinfo.sh                                         https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-iblinkinfo.sh
+wget -o ${DBNITRO}/bin/exa-racklayout.sh                                         https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-racklayout.sh
+wget -o ${DBNITRO}/bin/exa-versions.sh                                           https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-versions.sh
+wget -o ${DBNITRO}/bin/gg-afterstop.sh                                           https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-afterstop.sh
+wget -o ${DBNITRO}/bin/gg-info.sh                                                https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-info.sh
+wget -o ${DBNITRO}/bin/gg-start.sh                                               https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-start.sh
+wget -o ${DBNITRO}/bin/gg-status.sh                                              https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-status.sh
+wget -o ${DBNITRO}/bin/gg-stop.sh                                                https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-stop.sh
+wget -o ${DBNITRO}/bin/list-ohpatches.sh                                         https://raw.githubusercontent.com/freddenis/oracle-scripts/master/list-ohpatches.sh
+wget -o ${DBNITRO}/bin/lspatches.sh                                              https://raw.githubusercontent.com/freddenis/oracle-scripts/master/lspatches.sh
+wget -o ${DBNITRO}/bin/nfs-status.sh                                             https://raw.githubusercontent.com/freddenis/oracle-scripts/master/nfs-status.sh
+wget -o ${DBNITRO}/bin/oci-check-backups.sh                                      https://raw.githubusercontent.com/freddenis/oracle-scripts/master/oci-check-backups.sh
+wget -o ${DBNITRO}/bin/oraenv++                                                  https://raw.githubusercontent.com/freddenis/oracle-scripts/master/oraenv++
+wget -o ${DBNITRO}/bin/rac-mon.sh                                                https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-mon.sh
+wget -o ${DBNITRO}/bin/rac-on_all_db.sh                                          https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-on_all_db.sh
+wget -o ${DBNITRO}/bin/rac-status-rh6.sh                                         https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-status-rh6.sh
+wget -o ${DBNITRO}/bin/rac-status.sh                                             https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-status.sh
+wget -o ${DBNITRO}/bin/rac-status_suresh.sh                                      https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-status_suresh.sh
+wget -o ${DBNITRO}/bin/rman-backup.sh                                            https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rman-backup.sh
+wget -o ${DBNITRO}/bin/svc-set-failback-yes.sh                                   https://raw.githubusercontent.com/freddenis/oracle-scripts/master/svc-set-failback-yes.sh
+wget -o ${DBNITRO}/bin/svc-show-config.sh                                        https://raw.githubusercontent.com/freddenis/oracle-scripts/master/svc-show-config.sh
+wget -o ${DBNITRO}/bin/yal.sh                                                    https://raw.githubusercontent.com/freddenis/oracle-scripts/master/yal.sh
+wget -o ${DBNITRO}/statements/locks.sql                                          https://raw.githubusercontent.com/freddenis/oracle-scripts/master/locks.sql
+wget -o ${DBNITRO}/statements/pidsid.sql                                         https://raw.githubusercontent.com/freddenis/oracle-scripts/master/pidsid.sql
+wget -o ${DBNITRO}/statements/sidpid.sql                                         https://raw.githubusercontent.com/freddenis/oracle-scripts/master/sidpid.sql
 #
-wget -O ${DBNITRO}/.OracleMenu.sh                                https://raw.githubusercontent.com/dbaribas/dbnitro/main/OracleMenu.sh
-wget -O ${DBNITRO}/.Oracle_ASM_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ASM_Functions
-wget -O ${DBNITRO}/.Oracle_DBA_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_DBA_Functions
-wget -O ${DBNITRO}/.Oracle_RAC_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_RAC_Functions
-wget -O ${DBNITRO}/.Oracle_EXA_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_EXA_Functions
-wget -O ${DBNITRO}/.Oracle_ODG_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ODG_Functions
-wget -O ${DBNITRO}/.Oracle_OGG_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_OGG_Functions
-wget -O ${DBNITRO}/.Oracle_STR_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_STR_Functions
-wget -O ${DBNITRO}/.Oracle_PDB_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_PDB_Functions
-wget -O ${DBNITRO}/.Oracle_ODA_Functions                         https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_ODA_Functions
-wget -O ${DBNITRO}/.Oracle_WALL_Functions                        https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_WALL_Functions
-wget -O ${DBNITRO}/.Oracle_RMAN_Functions                        https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_RMAN_Functions
-wget -O ${DBNITRO}/Oracle_SQL_DBA_Info.sql                       https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_DBA_Info.sql
-wget -O ${DBNITRO}/Oracle_SQL_Report_v.3.0.1.sql                 https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_Report_v.3.0.1.sql
-wget -O ${DBNITRO}/Oracle_SQL_Check_Hugepages.sql                https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_Check_Hugepages.sql
-wget -O ${DBNITRO}/Oracle_DBA_Check_Hugepages.sh                 https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_DBA_Check_Hugepages.sh
-wget -O ${DBNITRO}/GoldenGateMonitor.sh                          https://raw.githubusercontent.com/dbaribas/dbnitro/main/GoldenGateMonitor.sh
-wget -O ${DBNITRO}/Oracle_SQL_Options_Packs_Usage_Statistics.sql https://raw.githubusercontent.com/dbaribas/dbnitro/main/Oracle_SQL_Options_Packs_Usage_Statistics.sql
+chmod a+x ${DBNITRO}/bin/OracleMenu.sh
+chmod g+w ${DBNITRO}/bin/OracleMenu.sh
+chmod 775 ${DBNITRO}/bin/OracleMenu.sh
 #
-chmod a+x ${DBNITRO}/.OracleMenu.sh
-chmod g+w ${DBNITRO}/.OracleMenu.sh
-chmod 775 ${DBNITRO}/.OracleMenu.sh
-#
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/asmdu.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/cell-status.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/change-password.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-howsmart.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-iblinkinfo.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-racklayout.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/exa-versions.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-afterstop.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-info.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-start.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-status.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/gg-stop.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/list-ohpatches.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/locks.sql
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/lspatches.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/nfs-status.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/oci-check-backups.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/oraenv++
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/pidsid.sql
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-mon.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-on_all_db.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-status-rh6.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-status.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rac-status_suresh.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/rman-backup.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/sidpid.sql
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/svc-set-failback-yes.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/svc-show-config.sh
-wget https://raw.githubusercontent.com/freddenis/oracle-scripts/master/yal.sh
-#
-chmod a+x ${DBNITRO}/*.sh
-chmod a+x ${DBNITRO}/oraenv++
+chmod a+x ${DBNITRO}/bin/*.sh
+chmod a+x ${DBNITRO}/bin/oraenv++
 #
 chmod -R 775 ${DBNITRO}/
 chown -R oracle.oinstall ${DBNITRO}/
@@ -124,16 +128,16 @@ chown -R oracle.oinstall ${DBNITRO}/
 #
 function SetUpPURGELOGS() {
 cd ${DBNITRO}/
-wget -O ${DBNITRO}/purgeLogs         https://raw.githubusercontent.com/dbaribas/dbnitro/main/purgeLogs
-wget -O /etc/cron.daily/purgeLogs.sh https://raw.githubusercontent.com/dbaribas/dbnitro/main/purgeLogs.sh
+wget -O ${DBNITRO}/bin/purgeLogs         https://raw.githubusercontent.com/dbaribas/dbnitro/main/purgeLogs
+wget -O /etc/cron.daily/purgeLogs.sh     https://raw.githubusercontent.com/dbaribas/dbnitro/main/purgeLogs.sh
 #
-chmod a+x ${DBNITRO}/purgeLogs
+chmod a+x ${DBNITRO}/bin/purgeLogs
 chmod a+x /etc/cron.daily/purgeLogs.sh
 #
-chmod -R 775 ${DBNITRO}/purgeLogs
+chmod -R 775 ${DBNITRO}/bin/purgeLogs
 chmod -R 775 /etc/cron.daily/purgeLogs.sh
 #
-chown -R oracle.oinstall ${DBNITRO}/purgeLogs
+chown -R oracle.oinstall ${DBNITRO}/bin/purgeLogs
 chown -R oracle.oinstall /etc/cron.daily/purgeLogs.sh
 #
 }
@@ -158,7 +162,7 @@ export PS1=\$'[ \${LOGNAME}@\h:\$(pwd): ]\$ '
 #
 echo " -- TO SELECT ANY ORACLE PRODUCT, JUST TYPE: db --"
 echo " -- IT WILL SHOW YOU ALL OPTIONS YOU CAN USE OR TYPE: HELP --"
-alias db='. ${DBNITRO}/.OracleMenu.sh'
+alias db='. ${DBNITRO}/bin/OracleMenu.sh'
 #
 umask 0022
 EOF
@@ -188,7 +192,7 @@ export PS1=\$'[ \${LOGNAME}@\h:\$(pwd): ]\$ '
 #
 echo " -- TO SELECT ANY ORACLE PRODUCT, JUST TYPE: db --"
 echo " -- IT WILL SHOW YOU ALL OPTIONS YOU CAN USE OR TYPE: HELP --"
-alias db='. ${DBNITRO}/.OracleMenu.sh'
+alias db='. ${DBNITRO}/bin/OracleMenu.sh'
 #
 umask 0022
 EOF
@@ -202,7 +206,7 @@ function SetUpOldOracle() {
 cat >> /home/oracle/.bash_profile <<EOF
 -- TO SELECT ANY ORACLE PRODUCT, JUST TYPE: db --            
 -- IT WILL SHOW YOU ALL OPTIONS YOU CAN USE OR TYPE: HELP --
-alias db='. ${DBNITRO}/.OracleMenu.sh'
+alias db='. ${DBNITRO}/bin/OracleMenu.sh'
 umask 0022
 EOF
 }
